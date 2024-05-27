e1ae422490892870342099687a9f26f96d1ed958
Soid23 <dorrismutahi@gmail.com>
2024-05-27 22:42:47 +0300

    database

--- database.sql
+++ database.sql
@@ -5,12 +5,18 @@ CREATE DATABASE IF NOT EXISTS Harvest_hub.db;
 USE Harvest_hub;
 
 -- Create users table
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL UNIQUE,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL  -- Consider storing hashed password for security
+CREATE TABLE users (
+    user_id INT NOT NULL,
+    username CHAR(10) NOT NULL,
+    firstname CHAR(10) NOT NULL,
+    lastname CHAR(10) NOT NULL,
+    password CHAR(10) NOT NULL,
+    email CHAR(10) NOT NULL,
+    role CHAR(10) NOT NULL,
+    status CHAR(50) NOT NULL,
+    profile_picture IMAGE NULL
 );
+
 
 -- Create products table
 CREATE TABLE IF NOT EXISTS products (
@@ -19,16 +25,23 @@ CREATE TABLE IF NOT EXISTS products (
   location TEXT NOT NULL,
   price REAL NOT NULL,
   seller_id INTEGER NOT NULL,
-  FOREIGN KEY (seller_id) REFERENCES users(id)
+  FOREIGN KEY (seller_id) REFERENCES user_(id)
 );
 
 -- Create orders table
 CREATE TABLE IF NOT EXISTS orders (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  order_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER NOT NULL,
+  buyer_id INTEGER NOT NULL,
+  seller_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
   quantity INTEGER NOT NULL,
+  order_date TIMESTAMP NOT NULL,
+  total_price INTEGER NOT NULL,
+  
   status TEXT NOT NULL,  -- Add options like 'pending', 'shipped', 'delivered'
-  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (buyer_id) REFERENCES users(id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (seller_id) REFERENCES users(user_id)
   FOREIGN KEY (product_id) REFERENCES products(id)
 );
